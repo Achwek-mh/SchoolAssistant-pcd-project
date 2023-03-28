c192231eaa69b8d7fec7b3583012e30f8b7c0535
Achwek-mh <achwekmhenni123@gmail.com>
2023-03-28 20:56:48 +0100

    new-commit

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,16 @@
-
-import mysql.connector
-import nltk
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
-from nltk.corpus import stopwords
+import jsonlines
+import pyttsx3 
+import numpy as np
+from sklearn.metrics import precision_score
 import speech_recognition as sr
-import pyttsx3
-import spacy
-import json
-from fuzzywuzzy import fuzz
+import mysql.connector
 
 
-nltk.download('stopwords')
-#nlp = spacy.load("xx_ent_wiki_sm")
 
-# Connect to MySQL database
+
+# Charger les données d'entraînement
 db = mysql.connector.connect(
     host="localhost",
     user="simplebot",
@@ -34,101 +29,117 @@ teacher_info = []
 for row in result:
     teachers.append(row[0])
     teacher_info.append(row[1])
-
-# Define function to preprocess data for SVM
-
+# Diviser les données en entrées (X) et sorties (y)
 
 
-def recognize_speech():
+
+# Définir les hyperparamètres
+C = 10.0  # Paramètre de régularisation
+kernel = 'linear'  # Noyau SVM
+gamma = 'scale'
+
+# Préparez les données pour l'entraînement SVM
+X = np.array(teachers)
+y = np.array(teacher_info)    
+x=X
+
+# Divisez les données en ensembles d'entraînement et de test
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Préparez les données pour l'entraînement SVM en utilisant NLP
+vectorizer = TfidfVectorizer()
+X_train_tfidf = vectorizer.fit_transform(X_train)
+
+# Créer le vecteur Tfidf
+#vectorizer = TfidfVectorizer()
+#X = vectorizer.fit_transform(X)
+
+# Entraînez SVM
+#clf = SVC(kernel='linear')
+#clf.fit(X_train_tfidf, y_train)
+
+# Entraîner le modèle SVM
+model = SVC(kernel='linear', C=10.0, gamma='scale')
+model.fit(X_train_tfidf, y_train)
+
+# Testez le chatbot
+X_test_tfidf = vectorizer.transform(X_test)
+y_pred = model.predict(X_test_tfidf)
+
+# Évaluez les performances du chatbot
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred, average='macro'))
+print("Recall:", recall_score(y_test, y_pred, average='macro'))
+print("F1 score:", f1_score(y_test, y_pred, average='macro'))
+
+# Initialiser le moteur de synthèse vocale
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')       #getting details of current voice
+
+
+# engine.setProperty('voice', voices[2h].id)  # Set to a French voice
+engine.setProperty('rate', 150)     # setting up new voice rate
+engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[26].id)   #changing index, changes voices. 1 for female
+
+def rec():
+   
     r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Please say something...")
-        r.adjust_for_ambient_noise(source, duration=0.2)
 
+    with sr.Microphone() as source:
+        print("Speak in French...")
         audio = r.listen(source, phrase_time_limit=5)
-    
+
     try:
         text = r.recognize_google(audio, language='fr-FR')
-        if text:
-
-            return(text)
-        else:
-            print("No speech detected")
+        return(text)
     except sr.UnknownValueError:
-        print("Speech recognition could not understand audio")
+        print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
-        
+    
 
 
+# Fonction pour prédire la réponse à une question donnée
+def predict_answer(question):
+    question_vec = vectorizer.transform([question])
+    if question_vec.nnz == 0:
+        return {"question": question,"question_data":"unknown", "answer": "unknown"}
+    answer = model.predict(question_vec)
+    pos=0
+    for a in y:
+        if a==answer:
+            question_data=x[pos]
+            break
+        pos+=1
+    return {"question": question,"question_data":question_data, "answer": answer[0]}
 
-def speak(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 125)  # adjust the speaking rate as desired
-    engine.say(text)
+# Fonction pour parler la réponse prédite
+def speak_answer(answer):
+    engine.say(answer)
     engine.runAndWait()
 
-def preprocess_data(data):
-    stop_words = set(stopwords.words('english'))
-    preprocessed_data = []
-    for sentence in data:
-        # Tokenize sentence and remove stopwords
-        tokens = nltk.word_tokenize(sentence.lower())
-        filtered_tokens = [token for token in tokens if token not in stop_words]
-        preprocessed_data.append(" ".join(filtered_tokens))
-    return preprocessed_data
-
-# Preprocess teacher information data
-teacher_info_processed = preprocess_data(teacher_info)
-
-
-# Create TfidfVectorizer and fit to teacher information data
-vectorizer = TfidfVectorizer()
-tfidf = vectorizer.fit_transform(teacher_info_processed)
-
-# Train SVC on teacher information data
-classifier = SVC(kernel='linear', C=10.0, gamma='scale')
-classifier.fit(tfidf, teachers)
-
-# Define function to get teacher name from query using SVC
-def get_teacher_name(query):
-   
-   
-    # If no match found, use SVC to predict teacher name
-
-    # Preprocess query
-    query_processed = preprocess_data([query])[0]
-
-    # Transform query using TfidfVectorizer
-    query_tfidf = vectorizer.transform([query_processed])
-
-    # Use SVC to predict teacher name
-    prediction = classifier.predict(query_tfidf)
-
-
-    print(prediction[0])
-    return prediction[0]
-
-
-# Define function to handle user input and output teacher information
-def chat():
-    print("Hi! How can I help you with teachers information?")
-    while True:
-        #query =  recognize_speech()
-        query=input("ask me \t")
-        if query == "bye":
-            break
-        teacher_name = get_teacher_name(query)
-        cursor.execute(f"SELECT info FROM ensinfo WHERE name = '{teacher_name}'")
-        result = cursor.fetchone()
-        if result is None:
-            print("Sorry, I couldn't find information on that teacher.")
-        else:
-
-            teacher_info = result[0]
-            speak(teacher_info)
-            print(teacher_info)
-
+# Exemple d'utilisation
+while True:
+    speak_answer("donner une question")
+    question = rec()
+    result = predict_answer(question)
+    print("Question :", result["question"])
+    print("Question Proposed:", result["question_data"])
+    print("Answer:", result["answer"])
+    print("Question is")
+    print(result["question"])
+    print("Question Proposed is")
+    print(result["question_data"])
+    print("Answer is")
+    speak_answer(result["answer"])
+    
+# Close database connection
+db.close()
 # Start chat
 chat()
 
