066bb61bfd31092127794c5f7e478a7c5cf97937
Achwek-mh <achwekmhenni123@gmail.com>
2023-03-28 12:46:10 +0100

    second-commit

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,4 @@
+
 import mysql.connector
 import nltk
 import numpy as np
@@ -6,21 +7,25 @@ from sklearn.svm import SVC
 from nltk.corpus import stopwords
 import speech_recognition as sr
 import pyttsx3
+import spacy
 import json
+from fuzzywuzzy import fuzz
+
 
 nltk.download('stopwords')
+#nlp = spacy.load("xx_ent_wiki_sm")
 
 # Connect to MySQL database
 db = mysql.connector.connect(
     host="localhost",
     user="simplebot",
-    password ="",
+    password ="22098.Achw",
     database="anotherbot"
 )
 
 # Get teacher information data from MySQL database
 cursor = db.cursor()
-cursor.execute("SELECT name, info FROM association")
+cursor.execute("SELECT name, info FROM ensinfo")
 result = cursor.fetchall()
 
 # Create lists of teacher names and their corresponding information
@@ -31,24 +36,29 @@ for row in result:
     teacher_info.append(row[1])
 
 # Define function to preprocess data for SVM
+
+
+
 def recognize_speech():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Please say something...")
-        audio = r.listen(source)
+        r.adjust_for_ambient_noise(source, duration=0.2)
+
+        audio = r.listen(source, phrase_time_limit=5)
     
     try:
-        text = r.recognize_google(audio, show_all=True)
+        text = r.recognize_google(audio, language='fr-FR')
         if text:
-            my_json_str = json.dumps(text)
+            """ my_json_str = json.dumps(text)
             data = json.loads(my_json_str)
             transcripts = data['alternative']
             # Sort the transcripts by confidence score in descending order
             sorted_transcripts = sorted(transcripts, key=lambda t:t.get('confidence', 0), reverse=True)
             # Get the highest confidence transcript
             highest_confidence_transcript = sorted_transcripts[0]['transcript']
-            print("Transcript with highest confidence: ", highest_confidence_transcript)
-            return(highest_confidence_transcript)
+            print("Transcript with highest confidence: ", highest_confidence_transcript) """
+            return(text)
         else:
             print("No speech detected")
     except sr.UnknownValueError:
@@ -56,13 +66,19 @@ def recognize_speech():
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
         
+""" def extract_entities(query):
+    doc = nlp(query)
+    entities = []
+    for ent in doc.ents:
+        entities.append(ent.text)
+    return entities """
 
 
-""" def speak(text):
+def speak(text):
     engine = pyttsx3.init()
-    engine.setProperty('rate', 150)  # adjust the speaking rate as desired
+    engine.setProperty('rate', 125)  # adjust the speaking rate as desired
     engine.say(text)
-    engine.runAndWait() """
+    engine.runAndWait()
 
 def preprocess_data(data):
     stop_words = set(stopwords.words('english'))
@@ -77,16 +93,21 @@ def preprocess_data(data):
 # Preprocess teacher information data
 teacher_info_processed = preprocess_data(teacher_info)
 
+
 # Create TfidfVectorizer and fit to teacher information data
 vectorizer = TfidfVectorizer()
 tfidf = vectorizer.fit_transform(teacher_info_processed)
 
 # Train SVC on teacher information data
-classifier = SVC(kernel='linear', C=1, gamma='scale')
+classifier = SVC(kernel='linear', C=10.0, gamma='scale')
 classifier.fit(tfidf, teachers)
 
 # Define function to get teacher name from query using SVC
 def get_teacher_name(query):
+   
+   
+    # If no match found, use SVC to predict teacher name
+
     # Preprocess query
     query_processed = preprocess_data([query])[0]
 
@@ -96,30 +117,41 @@ def get_teacher_name(query):
     # Use SVC to predict teacher name
     prediction = classifier.predict(query_tfidf)
 
-    # Return predicted teacher name
+    """ highest_score = 0
+    best_match = ""
+    for teacher in teachers:
+        score = fuzz.token_set_ratio(teacher.lower(), prediction[0].lower())
+        if score > highest_score:
+            highest_score = score
+            best_match = teacher
+
+    # Return best matched teacher name
+    print(best_match) """
+    print(prediction[0])
     return prediction[0]
+
 
 # Define function to handle user input and output teacher information
 def chat():
     print("Hi! How can I help you with teachers information?")
     while True:
-        """ query =  recognize_speech() """
-        query=input("ask me")
+        #query =  recognize_speech()
+        query=input("ask me \t")
         if query == "bye":
             break
         teacher_name = get_teacher_name(query)
-        cursor.execute(f"SELECT info FROM association WHERE name = '{teacher_name}'")
+        cursor.execute(f"SELECT info FROM ensinfo WHERE name = '{teacher_name}'")
         result = cursor.fetchone()
         if result is None:
             print("Sorry, I couldn't find information on that teacher.")
         else:
 
             teacher_info = result[0]
-            """ speak(teacher_info) """
+            speak(teacher_info)
             print(teacher_info)
 
 # Start chat
 chat()
 
 # Close database connection
-db.close()
+db.close() 
